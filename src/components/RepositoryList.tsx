82ce1195a743feb303d110f08ec7d3e11a1c4e0f
Sergio Rosa <scarlosjazz@gmail.com>
2022-03-17 18:26:36 -0400

    Structure with create-react-app

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,38 @@
+
+const repositoryName = 'unform2';
+
+
+export function RepositoryList(){
+
+  return(
+    <section className="repository-list">
+      <h1>Repository List</h1>
+      <ul>
+        <li>
+          <strong>repositoryName</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Access repository
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Access repository
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Access repository
+          </a>
+        </li>
+      </ul>
+    </section>
+
+  );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,52 @@
-const path = require('path');
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const reactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map': 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
+    hot: true,
+  },
+  plugins: [
+    isDevelopment && new reactRefreshWebpackPlugin(),
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ].filter(Boolean),
   module: {
-    rules:[
+    rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader','sass-loader'],
       }
     ]
-
   }
-};
+}
