a7ebd9126539758e156b12757b748dc61a235f47
Sergio Rosa <scarlosjazz@gmail.com>
2022-03-17 21:12:03 -0400

    Using the State Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { RepositoryList } from "./components/RepositoryList";
 import './styles/global.scss';
+import { Counter } from './components/Counter';
 
 
 export function App (){
-  return  <RepositoryList />
-  
+  return  (
+  <>
+    <RepositoryList />
+    <Counter />
+  </>
+  );
 }
